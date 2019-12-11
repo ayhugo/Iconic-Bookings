e2dc270e9b75cbd8249a20da94bd19b02ddfc19d
MacBook Ayre <hugo@MacBooks-MacBook-Pro.local>
2019-12-11 20:10:20 +1300

    alert delete

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RecipesService } from '../recipes.service';
 import { Recipe } from '../recipe.model';
+import { AlertController } from '@ionic/angular';
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.page.html',
-  styleUrls: ['./recipe-detail.page.scss'],
+  selector: "app-recipe-detail",
+  templateUrl: "./recipe-detail.page.html",
+  styleUrls: ["./recipe-detail.page.scss"]
 })
 export class RecipeDetailPage implements OnInit {
   loadedRecipe: Recipe;
@@ -13,23 +14,42 @@ export class RecipeDetailPage implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private recipesService: RecipesService,
-    private router: Router) { }
+    private router: Router,
+    private alertCrtl: AlertController
+  ) {}
 
   ngOnInit() {
     // emits new data everytime something chages
     this.activatedRoute.paramMap.subscribe(paramMap => {
-      if (!paramMap.has('recipeId')) {
+      if (!paramMap.has("recipeId")) {
         // redirect
         return;
       }
-      const recipeId = paramMap.get('recipeId');
+      const recipeId = paramMap.get("recipeId");
       this.loadedRecipe = this.recipesService.getRecipe(recipeId);
     });
   }
 
   onDeleteRecipe() {
-    this.recipesService.deleteRecipe(this.loadedRecipe.id);
-    this.router.navigate(['/recipes']);
-  }
+    this.alertCrtl.create({
+      header: "Are you sure?",
+      message: "Do you really want to delete the recipe?",
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'canel'
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            this.recipesService.deleteRecipe(this.loadedRecipe.id);
+            this.router.navigate(["/recipes"]);
+          }
+        }
+      ]
+    }).then(alertEl => {
+      alertEl.present();
+    });
 
+  }
 }
