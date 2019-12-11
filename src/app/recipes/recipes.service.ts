55cdbf9de7119f411065eb7f9dd459eb8eaf660c
MacBook Ayre <hugo@MacBooks-MacBook-Pro.local>
2019-12-11 20:03:18 +1300

    delete recipe

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -33,4 +33,10 @@ export class RecipesService {
       return recipe.id === recipeId
     })};
   }
+
+  deleteRecipe(recipeId: string) {
+    this.recipes = this.recipes.filter(recipe => {
+      return recipe.id !== recipeId;
+    });
+  }
 }
