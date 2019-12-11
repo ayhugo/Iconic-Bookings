2639b21e49883a45c8d69ff5efd296dd34420a3d
MacBook Ayre <hugo@MacBooks-MacBook-Pro.local>
2019-12-12 12:35:40 +1300

    created new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,18 @@ const routes: Routes = [
           './recipes/recipe-detail/recipe-detail.module#RecipeDetailPageModule'
       }
     ]
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+  },
+  {
+    path: 'places',
+    loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)
+  },
+  {
+    path: 'bookings',
+    loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)
   }
 ];
 
---
+++ src/app/places/offers/offers-bookings/offers-bookings.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-offers-bookings',
+  templateUrl: './offers-bookings.page.html',
+  styleUrls: ['./offers-bookings.page.scss'],
+})
+export class OffersBookingsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
