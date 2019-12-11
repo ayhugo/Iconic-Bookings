977dbac085556606a5b6a2ed0fd76736ac029a0b
MacBook Ayre <hugo@MacBooks-MacBook-Pro.local>
2019-12-12 12:17:21 +1300

    delete lifecycle hooks

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RecipesService } from './recipes.service';
 import { Recipe } from './recipe.model';
 @Component({
@@ -6,12 +6,32 @@ import { Recipe } from './recipe.model';
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit, OnDestroy {
   recipes: Recipe[];
   constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {
+    console.log('ngOnIt');
+  }
+
+  ionViewWillEnter() {
+    console.log('willenter');
     this.recipes = this.recipesService.getAllRecipes();
   }
 
+  ionViewDidEnter() {
+    console.log('didentger');
+  }
+
+  ionViewWillLeave() {
+    console.log('eilleave');
+  }
+
+  ionViewDidlLeave() {
+    console.log('didleave');
+  }
+
+  ngOnDestroy(){
+
+  }
 }
